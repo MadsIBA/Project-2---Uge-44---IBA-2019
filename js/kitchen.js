6fd5dfcf8dcc9c6a4024ae0ef9ae1cc62fc90598
mads <mads2097@gmail.com>
2019-10-29 16:39:38 +0100

    Extra Canvas for testing

--- js/kitchen.js
+++ js/kitchen.js
@@ -1,1 +1,20 @@
-console.log("Hej");
+var canvas = document.getElementById("kitchen")
+canvas.width = 800;
+canvas.height = 400;
+
+//Forkortelse for context
+var ctx = canvas.getContext("2d");
+
+
+//Køkken elementer array
+var kicthenElement = [tools, table];
+
+var tools = ctx.fillRect(20, 20, 40, 60)
+var table = ctx.fillRect(20, 100, 60, 40)
+
+/*
+//Køkken elementer til canvas
+for (var i = 0; i < 2; i++) {
+    kitchenElemenet = [i];
+    }
+*/
