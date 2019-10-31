324868ba4546b6813e05dee992cf0e0ea61d4ef5
mads <mads2097@gmail.com>
2019-10-31 19:35:31 +0100

    Added Form Reset Button - Tanuki

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let Canvas = {
 
         let height = document.getElementById("height").value;
         let width = document.getElementById("width").value;
-        
+
         this.canvas.height = height;
         this.canvas.width = width;
     },
@@ -99,7 +99,7 @@ let Circle = {
 
 
 let initialize = function () {
-    
+
     // Create two canvases and put two shapes into the right one via an array
     mycv1 = Object.create(Canvas);
     mycv1.init('myCanvas1', 'transparent');
@@ -115,21 +115,21 @@ let initialize = function () {
     shape3.init(mycv1, 160, 180, 60, 60, 'red');
     // Create Doors objects
     let shape4 = Object.create(Circle);
-    shape4.init(mycv1, 60, 190, 40, 180 * Math.PI/180, 285 * Math.PI/180, false, 'black');
+    shape4.init(mycv1, 60, 190, 40, 180 * Math.PI / 180, 285 * Math.PI / 180, false, 'black');
     let shape5 = Object.create(Circle);
-    shape5.init(mycv1, 90, 190, 40, 0 * Math.PI/180, 255 * Math.PI/180, true, 'black');
+    shape5.init(mycv1, 90, 190, 40, 0 * Math.PI / 180, 255 * Math.PI / 180, true, 'black');
     let shape6 = Object.create(Circle);
-    shape6.init(mycv1, 60, 200, 40, 180 * Math.PI/180, 75 * Math.PI/180, true, 'black');
+    shape6.init(mycv1, 60, 200, 40, 180 * Math.PI / 180, 75 * Math.PI / 180, true, 'black');
     let shape7 = Object.create(Circle);
-    shape7.init(mycv1, 90, 200, 40, 0 * Math.PI/180, 105 * Math.PI/180, false, 'black');
+    shape7.init(mycv1, 90, 200, 40, 0 * Math.PI / 180, 105 * Math.PI / 180, false, 'black');
     let shape8 = Object.create(Circle);
-    shape8.init(mycv1, 70, 60, 40, 270 * Math.PI/180, 165 * Math.PI/180, true, 'black');
+    shape8.init(mycv1, 70, 60, 40, 270 * Math.PI / 180, 165 * Math.PI / 180, true, 'black');
     let shape9 = Object.create(Circle);
-    shape9.init(mycv1, 80, 60, 40, 270 * Math.PI/180, 15 * Math.PI/180, false, 'black');
+    shape9.init(mycv1, 80, 60, 40, 270 * Math.PI / 180, 15 * Math.PI / 180, false, 'black');
     let shape10 = Object.create(Circle);
-    shape10.init(mycv1, 70, 90, 40, 90 * Math.PI/180, 195 * Math.PI/180, false, 'black');
+    shape10.init(mycv1, 70, 90, 40, 90 * Math.PI / 180, 195 * Math.PI / 180, false, 'black');
     let shape11 = Object.create(Circle);
-    shape11.init(mycv1, 80, 90, 40, 90 * Math.PI/180, 345 * Math.PI/180, true, 'black');
+    shape11.init(mycv1, 80, 90, 40, 90 * Math.PI / 180, 345 * Math.PI / 180, true, 'black');
     // Put objects in array
     shapes.push(shape1);
     shapes.push(shape2);
@@ -166,12 +166,12 @@ let select = function (ev) {
 
         cx.beginPath();
         cx.rect(shapes[i].x, shapes[i].y, shapes[i].width, shapes[i].height);
-        
+
         cx.arc(shapes[i].x, shapes[i].y, shapes[i].radius, shapes[i].sAngle, shapes[i].eAngle, shapes[i].rotation);
         cx.lineTo(shapes[i].lx1, shapes[i].ly1);
         cx.closePath();
-        
-        let bb = this.getBoundingClientRect();    // get canvas as std obj
+
+        let bb = this.getBoundingClientRect(); // get canvas as std obj
         // convert mouse coordinates to canvas coordinates
         let x = (ev.clientX - bb.left) * (this.width / bb.width);
         let y = (ev.clientY - bb.top) * (this.height / bb.height);
@@ -179,27 +179,27 @@ let select = function (ev) {
             // we're in a loop, is this array element the 
             // one we clicked? If yes click in other canvas
             mycv2.canvas.addEventListener('click', function placeInRoom(e) {
-                    let bb1 = this.getBoundingClientRect();    // yes
-                    // other canvas as std object
-                    // convert mouse coordinates to canvas coordinates
-                    let x1 = (e.clientX - bb1.left) * (this.width / bb1.width);
-                    let y1 = (e.clientY - bb1.top) * (this.height / bb1.height);
-                    let obj = Object.create(Shape); // create new obj Shape
-                    // with adapted properties
-                    obj.init(mycv2, x1, y1, 
-                                shapes[i].width, shapes[i].height,
-                                shapes[i].color);
-                    let obj2 = Object.create(Circle); // create new obj Circle
-                    obj2.init(mycv2, x1, y1,
-                                shapes[i].radius, shapes[i].sAngle, shapes[i].eAngle, shapes[i].rotation,
-                                shapes[i].color);
-                    othershapes.push(obj, obj2);
-                    
-                    mycv1.canvas.removeEventListener('click', select);
-                    repeater(mycv2, othershapes);
-                    mycv2.canvas.removeEventListener('click', placeInRoom);
-                    mycv1.canvas.addEventListener('click', select);
-                });
+                let bb1 = this.getBoundingClientRect(); // yes
+                // other canvas as std object
+                // convert mouse coordinates to canvas coordinates
+                let x1 = (e.clientX - bb1.left) * (this.width / bb1.width);
+                let y1 = (e.clientY - bb1.top) * (this.height / bb1.height);
+                let obj = Object.create(Shape); // create new obj Shape
+                // with adapted properties
+                obj.init(mycv2, x1, y1,
+                    shapes[i].width, shapes[i].height,
+                    shapes[i].color);
+                let obj2 = Object.create(Circle); // create new obj Circle
+                obj2.init(mycv2, x1, y1,
+                    shapes[i].radius, shapes[i].sAngle, shapes[i].eAngle, shapes[i].rotation,
+                    shapes[i].color);
+                othershapes.push(obj, obj2);
+
+                mycv1.canvas.removeEventListener('click', select);
+                repeater(mycv2, othershapes);
+                mycv2.canvas.removeEventListener('click', placeInRoom);
+                mycv1.canvas.addEventListener('click', select);
+            });
         } else {
             // window.alert("nohit: "+x+","+y);
         }
